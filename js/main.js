160770948b7d91fdce52de5a42570d1ee731fd9a
brnbrns <brianburns48@gmail.com>
2015-08-02 12:07:51 -0500

    Added navbar and fixed several problems

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-// Will contain nav bar and effects
+/**************
+* Brian Burns *
+*   main.js   *
+* brnbrns.co  *
+**************/
+
+$(document).ready(function() {
+  // Scroll down to About on arrow click
+  $('.downarrow').click(function() {
+    $('html, body').animate({
+      scrollTop: ($('.about').offset().top) - 150
+    }, 750);
+  });
+
+  // Persistent navbar when scrolling down the page
+  $(window).on('scroll', function() {
+    var yScrollPos = window.pageYOffset;
+    var scrollPosTest = 150;
+
+    if (yScrollPos > scrollPosTest) {
+      $('.navbar').fadeIn();
+    } else {
+      $('.navbar').fadeOut();
+    }
+  });
+});
